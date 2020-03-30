d06fc87f2f6a86892262b73cd880e67870188db3
Nikita <jonyroww@gmail.com>
2020-03-30 12:59:32 +0300

    Added foreign key to categories

---
+++ src/migrations/1585561665488-cadegories-foreign-key.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from "typeorm";
+
+export class cadegoriesForeignKey1585561665488 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createForeignKey(
+      "categories",
+      new TableForeignKey({
+        name: "FK_parent_category_id_id",
+        columnNames: ["parent_category_id"],
+        referencedTableName: "categories",
+        referencedColumnNames: ["id"],
+        onDelete: "CASCADE"
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropForeignKey("categories", "FK_parent_category_id_id");
+  }
+}
