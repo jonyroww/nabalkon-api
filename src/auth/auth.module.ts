5b88c6836bc4c846af5ef7994390d4610b9aa093
Nikita <jonyroww@gmail.com>
2020-04-07 15:11:57 +0300

    Added strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserRepository } from "../users/repositories/User.repository";
+import { PhoneVerificationRepository } from "../phone-verification/repositories/Phone-verification.repository";
+import { ConfigModule } from "./../config/config.module";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserRepository, PhoneVerificationRepository]),
+    ConfigModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { makeError } from "../common/errors";
+import bcrypt from "bcrypt";
+import { UserRepository } from "../users/repositories/User.repository";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userRepository: UserRepository) {}
+  async validateUser(phone: string, password: string) {
+    const user = await this.userRepository.findOne({ phone: phone });
+    if (user) {
+      if (user.deleted_at) {
+        throw makeError("USER_NOT_FOUND");
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (isPasswordValid) {
+        return user;
+      } else {
+        throw makeError("WRONG_PASSWORD");
+      }
+    } else {
+      throw makeError("USER_NOT_FOUND");
+    }
+  }
+}
