25ce864cc84c88dd64e506e422297eed5c8b6b11
jonyroww <jonyroww@gmail.com>
2020-05-08 15:23:02 +0300

    prettier fix

--- src/favorite-ads/entities/favorite-ads.entity.ts
+++ src/favorite-ads/entities/favorite-ads.entity.ts
@@ -1,57 +1,46 @@
-import {
-  Entity,
-  PrimaryColumn,
-  Column,
-  OneToMany,
-  ManyToMany,
-  JoinTable,
-  ManyToOne,
-  JoinColumn,
-} from "typeorm";
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { User } from "../../users/entities/User.entity";
-import { Ads } from "../../ads/entities/Ads.entity";
-import { FavoriteAdGroup } from "../../favorite-ads-group/entities/ad-group.entity";
+import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { FavoriteAdGroup } from '../../favorite-ads-group/entities/ad-group.entity';
 
-@Entity("favorite_ads")
+@Entity('favorite_ads')
 export class FavoriteAd {
   @ApiProperty()
   @PrimaryColumn({
-    type: "int",
+    type: 'int',
     generated: true,
     readonly: true,
   })
   id: number;
 
   @ApiProperty({
-    type: "string",
-    example: "2019-11-22T16:03:05Z",
+    type: 'string',
+    example: '2019-11-22T16:03:05Z',
     nullable: false,
   })
   @Column({
     nullable: false,
-    type: "timestamp with time zone",
+    type: 'timestamp with time zone',
   })
   created_at: Date;
 
-  @ApiProperty({ type: "int" })
-  @Column({ type: "int" })
+  @ApiProperty({ type: 'int' })
+  @Column({ type: 'int' })
   user_id: number;
 
-  @ApiProperty({ type: "int" })
-  @Column({ type: "int" })
+  @ApiProperty({ type: 'int' })
+  @Column({ type: 'int' })
   ad_id: number;
 
-  @ApiProperty({ type: "int" }) 
-  @Column({ type: "int" })
+  @ApiProperty({ type: 'int' })
+  @Column({ type: 'int' })
   group_id: number;
 
   @ApiProperty()
   @ManyToOne(
     () => FavoriteAdGroup,
     (group: FavoriteAdGroup) => group.favorite_ads,
-    {eager:true}
+    { eager: true },
   )
-  @JoinColumn({ name: "group_id" })
+  @JoinColumn({ name: 'group_id' })
   group: FavoriteAdGroup;
 }
--- src/favorite-ads/favorite-ads.controller.ts
+++ src/favorite-ads/favorite-ads.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { Controller, Post, Param, Body, Get } from '@nestjs/common';
 import { FavoriteAdsService } from './favorite-ads.service';
 import { UserIdDto } from '../common/dto/user-id.dto';
 import { CreateFavoriteAdDto } from './dto/create-favorite-ad-body.dto';
 
 @Controller()
 export class FavoriteAdsController {
-    constructor(private favoriteAdsService: FavoriteAdsService){}
+  constructor(private favoriteAdsService: FavoriteAdsService) {}
 
-    @Post("users/:userId/favorites/ads")
-    createFavoriteAd(@Param() params:UserIdDto, @Body() body:CreateFavoriteAdDto){
-        return this.favoriteAdsService.createFavoriteAd(params, body);
-    }
+  @Post('users/:userId/favorites/ads')
+  createFavoriteAd(
+    @Param() params: UserIdDto,
+    @Body() body: CreateFavoriteAdDto,
+  ) {
+    return this.favoriteAdsService.createFavoriteAd(params, body);
+  }
 }
